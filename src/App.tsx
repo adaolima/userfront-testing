cd320847b113833d1d0b19171480672c0cff55f4
Adão Lima <adaoolima@gmail.com>
2022-09-29 18:54:46 +0100

    Downgraded react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Routes as Switch,
+  Switch,
   Route,
   NavLink
 } from 'react-router-dom';
+
+// import './App.css';
 
 import Login from './pages/Login'
 import Home from './pages/Home'
@@ -12,20 +14,22 @@ import Home from './pages/Home'
 function App() {
   return (
     <Router>
-      <div className="page">
+      <div>
         <nav>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/">Home</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/login">Login</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/reset">Reset</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/dashboard">Dashboard</NavLink>
-          </li>
+          <ul>
+            <li className="nav-item">
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/login">Login</NavLink>
+            </li>
+            {/* <li className="nav-item">
+              <Link className="nav-link" to="/reset">Reset</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/dashboard">Dashboard</Link>
+            </li> */}
+          </ul>
         </nav>
         <Switch>
           <Route path="/login">
