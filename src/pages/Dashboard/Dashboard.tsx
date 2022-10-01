23891ecddd5835ff609beb941b9492585af854e3
Adão Lima <adaoolima@gmail.com>
2022-10-01 14:24:11 +0100

    Added redirect if user isn't logged

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import {
+  Route,
+  Redirect,
+  RouteChildrenProps
+} from 'react-router-dom';
+import Userfront from '@userfront/react';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>
+  const renderFn = ({ location }: RouteChildrenProps) => {
+    if (!Userfront.accessToken) {
+      return (
+        <Redirect
+         to={{
+          pathname: '/',
+          state: { from: location }
+         }}
+        />
+      )
+    }
+
+    const userData = JSON.stringify(Userfront.user, null, 2);
+
+    return (
+      <>
+        <h1>Dashboard</h1>
+        <pre> { userData } </pre>
+        <button onClick={Userfront.logout}>Logout</button>
+      </>
+    )
+  }
+  return <Route render={renderFn}/>
 }
 
 export default Dashboard;
