43192a4ec194d7eae5ed60f810f363601d574e0b
Adão Lima <adaoolima@gmail.com>
2022-10-01 14:24:25 +0100

    Added forms

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Userfront from '@userfront/react';
+
+Userfront.init("pn445q8n");
+
+const LoginForm = Userfront.build({
+  toolId: "rddkrr"
+});
+
 
 const Login: React.FC = () => {
-  return <h1>Login</h1>
+  return (
+    <>
+      <h1>Login</h1>
+      <LoginForm />
+    </>
+  )
 }
 
 export default Login;
--- src/pages/PasswordReset/PasswordReset.tsx
+++ src/pages/PasswordReset/PasswordReset.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Userfront from '@userfront/react';
+
+Userfront.init("pn445q8n");
+
+const PasswordResetForm = Userfront.build({
+  toolId: "nmmlna"
+});
+
 
 const PasswordReset: React.FC = () => {
-  return <h1>PasswordReset</h1>
+  return (
+    <>
+      <h1>PasswordReset</h1>
+      <PasswordResetForm />
+    </>
+  )
 }
 
 export default PasswordReset;
